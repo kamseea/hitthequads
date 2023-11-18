2fd25db7a52d0e53ae0ffb649073b1f424a17676
kamseea <138553231+kamseea@users.noreply.github.com>
2023-11-18 21:22:40 +0800

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
 <footer class="bg-light text-center text-lg-start">
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2023 Copyright |  The Quads | <a href="http://localhost/quads/index.php/napwc/admin"> Admin </a>
+    © 2023 Copyright |  The Quads | <a href="http://localhost/quads/index.php/napwc/admin"  style="text-decoration: none; color: black;"> Admin </a>
   </div>
   <!-- Copyright -->
 </footer>
