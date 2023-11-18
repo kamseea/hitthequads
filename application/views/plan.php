0350f6867dbcd216cf85d7dd0c245390ccf80173
kamseea <138553231+kamseea@users.noreply.github.com>
2023-11-18 21:22:58 +0800

    Update plan.php

--- application/views/plan.php
+++ application/views/plan.php
@@ -5,18 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+     
         h1{
             color:#204c37;
             text-align:center;
             font-weight:700;
-            padding-top:20px;
+            padding-top:70px;
         }
+
+        h3{
+            text-align:center;
+            padding-bottom:20px;
+        }
+        .c{
+            text-align:center;
+        }
+        
+        .button-link {
+            display: block;
+            padding: 10px 20px;
+            margin: 10px auto; 
+            text-align: center;
+            text-decoration: none;
+            color: #fff;
+            background-color: #204c37;
+            border-radius: 5px;
+            max-width:400px;
+        }
+
+        .button-link:hover {
+            background-color: grey;
+        }
+
         </style>
 </head>
 <body>
     <h1> Plan your Visit </h1>
     <h3> Make a Reservation </h3>
-    <div>
+    <div class="c">
     <a href="http://localhost/quads/index.php/napwc/fieldtrip" class="button-link">Field Trip</a>
     </div>
     <div>
